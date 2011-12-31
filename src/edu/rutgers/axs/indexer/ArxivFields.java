aae6779c83f95ad20a286146beed1045c1f558af
vmenkov <vmenkov@86fa5bc5-d9b1-0410-b350-fe26eda8ebdd>
2011-12-31 06:35:51 +0000

    forgot one file

---
+++ src/edu/rutgers/axs/indexer/ArxivFields.java
@@ -0,0 +1,13 @@
+package edu.rutgers.axs.indexer;
+
+/** Names of fields we use in the Lucene index */
+public class ArxivFields {
+    /** This field contains arXiv article id */
+    final static public String PAPER = "paper";    
+    final static public String 
+	ARTICLE = "article", // body
+	ABSTRACT = "abstract",
+	TITLE="title",
+	AUTHORS="authors",
+	COMMENTS="comments";
+}
