f6c46c913ca3b1da897b9a3aae80852ccc7ccc2a
vmenkov <vmenkov@86fa5bc5-d9b1-0410-b350-fe26eda8ebdd>
2012-11-28 19:36:33 +0000

    one more file

---
+++ src/edu/rutgers/axs/web/Participation.java
@@ -0,0 +1,54 @@
+package edu.rutgers.axs.web;
+
+import java.io.*;
+import java.util.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import javax.persistence.*;
+
+import edu.rutgers.axs.sql.*;
+
+public class Participation extends ResultsBase {
+
+    Invitation inv = null;
+    public String code=null;
+    public boolean bernoulli = false;
+
+    public Participation(HttpServletRequest _request, HttpServletResponse _response) {
+	super(_request,_response);
+	code = request.getParameter("code");   
+	if (code==null || code.equals("") || code.equals("null")) code=null;
+
+	if (code==null) {
+	    error=true;
+	    errmsg="Presently, new user enrollment is by invitation only. You need to receive an invitation, and then register using the URL provided";
+	    return;
+	}
+	EntityManager em=null;
+	try {
+	    em = sd.getEM();
+	    inv = Invitation.findByCode(em, code); 
+	    if (inv==null) {
+		error=true;
+		errmsg="Invitation code " + code + " is not valid";
+		return;		
+	    }
+	    if (!inv.validityTest(em)) {
+		error=true;
+		errmsg="We are sorry, but invitation no. " + inv.getId() + " (code=" + inv.getCode() + ") is presently closed, either because we have already recruited the desired number of users, or because the enrollment period has ended. You may want to receive a new invitation from the organizers of the experiment";
+		return;		
+	    }
+
+	    bernoulli = inv.getProgram().needBernoulli();
+
+	}  catch (Exception _e) {
+	    setEx(_e);
+	} finally {
+	    ensureClosed(em);
+	}
+    }
+
+
+}
