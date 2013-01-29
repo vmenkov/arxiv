6e9a287c5bd6f283beb97f77886d41bd06f7bbe2
vmenkov <vmenkov@86fa5bc5-d9b1-0410-b350-fe26eda8ebdd>
2013-01-29 03:25:59 +0000

    ver 0.2.023

--- src/edu/rutgers/axs/Version.java
+++ src/edu/rutgers/axs/Version.java
@@ -10,8 +10,8 @@ package  edu.rutgers.axs;
  */
 public class Version {
     /** The version number of the applications */
-    public final static String version = "0.2.022";
-    public final static String date = "2012-01-09";
+    public final static String version = "0.2.023";
+    public final static String date = "2012-01-28";
 
     public static String getVersion() { return version; }
 
