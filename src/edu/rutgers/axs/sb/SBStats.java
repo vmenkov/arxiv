69cca099fe35df450377faafb7aa8e7626029d82
vmenkov <vmenkov@86fa5bc5-d9b1-0410-b350-fe26eda8ebdd>
2014-08-26 16:39:24 +0000

    stats, web UI

--- src/edu/rutgers/axs/sb/SBStats.java
+++ src/edu/rutgers/axs/sb/SBStats.java
@@ -3,15 +3,15 @@ package edu.rutgers.axs.sb;
 import java.io.*;
 import java.util.*;
 import java.text.*;
-//import java.net.*;
-//import java.util.regex.*;
 import javax.persistence.*;
 
 import edu.rutgers.axs.sql.*;
 import edu.rutgers.axs.web.*;
-//import edu.rutgers.axs.indexer.*;
 
-/** Handling statistics for SB experiments */
+/** Handling statistics for SB experiments, as proposed by Paul Kantor in
+    a 2014-06-04 email ("Session Buddy. First round of experiments") and refined
+    in a later email discussion.
+ */
 public class SBStats {
 
     static private Integer long2int(Long x) {
@@ -37,20 +37,17 @@ public class SBStats {
 	else  q.setParameter("uname", uname);
 	q.setParameter("type", edu.rutgers.axs.sql.Action.Source.SB);
 
-	//	HashMap<Long,Integer> h= new HashMap<Long,Integer> ();
 	Vector<Long[]> v= new 	Vector<Long[]>();
 	List list = q.getResultList();
 	for(Object o: list) {
 	    Object[] z = (Object[]) o;
-	    //h.put((Long)(z[0]), long2int((Long)(z[1])));
 	    v.add(new Long[] { (Long)(z[0]), (Long)(z[1])});
 	}
 	return v;
- 
-    }
+     }
 
     /** Report on one web session */
-    static public void sessionStats(PrintStream out, EntityManager em, long sqlSessionID) {
+    static public void sessionStats(PrintStream out, EntityManager em, long sqlSessionID, boolean html) {
 
 	RatedActionHistory his = new RatedActionHistory(em, sqlSessionID);
 	String query = "select x from PresentedList x where x.session = :sid and x.type = :type and x.hidden=:h order by x.id";
@@ -59,16 +56,13 @@ public class SBStats {
 	q.setParameter("type", edu.rutgers.axs.sql.Action.Source.SB);
 	q.setParameter("sid", sqlSessionID);
 	q.setParameter("h", Boolean.FALSE);
-	Vector<PresentedList> v = new	Vector<PresentedList>();
+	Vector<PresentedList> v1 = new	Vector<PresentedList>();
 	List list = q.getResultList();
 	for(Object o: list) {
-	    v.add((PresentedList)o);
+	    v1.add((PresentedList)o);
 	}
-	out.println("Displayed lists for session " +  sqlSessionID);
-	for(PresentedList x: v) {
-	    out.println("PresentedList " + x.getId() + ", sbMethod=" + x.getSbMethod());
-	    plStats(out, x, his);
-	}
+
+
 	
 	//query = "select x from PresentedList x where x.session = :sid and x.type = :type and x.hidden=:h order by x.id";
 	Query q2 = em.createQuery(query);
@@ -76,15 +70,53 @@ public class SBStats {
 	q2.setParameter("sid", sqlSessionID);
 	q2.setParameter("h", Boolean.TRUE);
 	list = q2.getResultList();
-	v = new	Vector<PresentedList>();
+	Vector<PresentedList> v2 = new	Vector<PresentedList>();
 	for(Object o: list) {
-	    v.add((PresentedList)o);
+	    v2.add((PresentedList)o);
 	}
-	out.println("Hidden lists for session " +  sqlSessionID);
-	for(PresentedList x: v) {
-	    out.println("PresentedList " + x.getId() + ", sbMethod=" + x.getSbMethod());
-	    plStats(out, x, his);
 
+	if (html) {
+	    out.println("<table border=1>");
+	    for(PresentedList x: v1) {
+		out.println("<tr>");
+		out.println("<td><pre>");
+		//out.println("PresentedList " + x.getId() + ", sbMethod=" + x.getSbMethod());
+		plStats(out, x, his);
+		out.println("</pre></td>");
+
+		// show the two underlying lists in the other two columns 
+		long u[] = new long[] { x.getMergePlid1(), x.getMergePlid2()};
+		for(long plid : u) {
+		    if (plid==0) continue;
+		    PresentedList y = null; 
+		    for(PresentedList z: v2) {
+			if (z.getId() == plid) { y = z; break; }
+		    }
+		    if (y==null) continue;
+		    out.println("<td><pre>");
+		    //out.println("PresentedList " + y.getId() + ", sbMethod=" + y.getSbMethod());
+		    plStats(out, y, his);
+		    out.println("</pre></td>");	    
+		}
+
+
+		out.println("</tr>");
+	    }
+	    	    
+	    out.println("</table>");
+	} else {
+	    out.println("Displayed lists for session " +  sqlSessionID);
+	    for(PresentedList x: v1) {
+		out.println("PresentedList " + x.getId() + ", sbMethod=" + x.getSbMethod());
+		plStats(out, x, his);
+	    }
+	    
+	    out.println("Hidden lists for session " +  sqlSessionID);
+	    for(PresentedList x: v2) {
+		out.println("PresentedList " + x.getId() + ", sbMethod=" + x.getSbMethod());
+		plStats(out, x, his);
+		
+	    }
 	}
     }
 
@@ -99,14 +131,18 @@ $$
     static void plStats(PrintStream out, PresentedList pl, RatedActionHistory his) {
 	out.println("PresentedList no. " + pl.getId());
 	out.println("sbMethod= " + pl.getSbMethod());
+	out.println(pl.getTime());
 	Vector<PresentedListEntry> docs = pl.getDocs();
-	double ow = 1;
+	double ow = 1, sum=0;
 	for(int i=0; i<docs.size(); i++) {	    
 	    String aid = docs.elementAt(i).getAid();
 	    Integer o = his.get(aid);  // page score
 	    int v = (o==null) ? 0 : o;
 	    out.println(aid + "\t" + v);
+	    sum += v / ow;
+	    if (v < 1) ow += 1;	    
 	}
+	out.println("Score(L)=" + sum);
     }
 
     /** Looks up the "score" for pages based on user actions within
@@ -159,7 +195,7 @@ $$
 		out.println("-----------------------------------------------");
 		out.println("Session " +z[0]+", " + z[1] + " lists presented");
 		long sqlSessionID=z[0];
-		sessionStats(out, em, sqlSessionID);
+		sessionStats(out, em, sqlSessionID, false);
 	    }
 
 
--- src/edu/rutgers/axs/web/SBStatsServlet.java
+++ src/edu/rutgers/axs/web/SBStatsServlet.java
@@ -3,22 +3,17 @@ package edu.rutgers.axs.web;
 import java.io.*;
 import java.util.*;
 import java.text.*;
-import java.lang.reflect.*;
-import java.util.regex.*;
 import java.net.URLEncoder;
-
 
 import javax.servlet.*;
 import javax.servlet.http.*;
-
 import javax.persistence.*;
 
 import edu.rutgers.axs.sql.*;
 import edu.rutgers.axs.sb.SBStats;
 
-
- /** Web interface to SBStats
- */
+/** Web interface to SBStats
+*/
 public class SBStatsServlet extends HttpServlet {
 
   public void service(HttpServletRequest request,HttpServletResponse response) {
@@ -33,9 +28,7 @@ public class SBStatsServlet extends HttpServlet {
 	    QueryServlet.Format format = (QueryServlet.Format)Tools.getEnum(request, QueryServlet.Format.class, QueryServlet.FORMAT, QueryServlet.Format.HTML);
 	    boolean html = (format==QueryServlet.Format.HTML);
 
-
 	    boolean toFile = ((QueryServlet.ToFile)Tools.getEnum(request, QueryServlet.ToFile.class, QueryServlet.TO_FILE, QueryServlet.ToFile.Browser)).toFile();
-
 
 	    SessionData sd =  SessionData.getSessionData(request);
 
@@ -50,7 +43,6 @@ public class SBStatsServlet extends HttpServlet {
 	    }
 
 	    em = sd.getEM();
-
 
 	    response.setContentType(html? "text/html" :
 				    toFile? "text/csv" : "text/plain");
@@ -68,6 +60,8 @@ public class SBStatsServlet extends HttpServlet {
 
 	    String p = html? "<p>":"";
 	    String ep = html? "</p>":"";
+	    String h2 = html? "<h2>":"";
+	    String eh2 = html? "</h2>":"";
 
 	    if (html) {
 		out.println("<html>");
@@ -81,10 +75,10 @@ public class SBStatsServlet extends HttpServlet {
 
 	    Vector<Long[]> v= SBStats.listSessionsForUser( em, 0, uname);
 	    for(Long[] z : v) {
-		out.println("<h2>Session " +z[0]+", " + z[1] + " lists presented</h2>");
+		out.println(h2 + "Session " +z[0]+", " + z[1] + " list(s) presented"+eh2);
 		long sqlSessionID=z[0];
 		out.println("<pre>");
-		SBStats.sessionStats(out, em, sqlSessionID);
+		SBStats.sessionStats(out, em, sqlSessionID, html);
 		out.println("</pre>");
 	    }
 
