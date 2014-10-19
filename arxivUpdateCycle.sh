99c3ecc092bf7ff4fe5881ffc3d5178974d9886d
vmenkov <vmenkov@86fa5bc5-d9b1-0410-b350-fe26eda8ebdd>
2014-10-19 04:34:22 +0000

    now can be used by other users (such as "tomcat")

--- arxivUpdateCycle.sh
+++ arxivUpdateCycle.sh
@@ -7,7 +7,12 @@
 #-- * ArticleAnalyzer, which updates article stats in the database
 #-- * TaskMaster, which will keep running for the next 24 hours, serving
 #---  suggestion list generation requests.
-#-----------------------------------------------------------------------------
+#
+# There is a provision to explicitly specify the value of $home (via
+# the 1st command-line argument), which is used as a base for finding
+# jar files etc. This is for the convenience of running the script as
+# a different user (e.g. with crontab)
+# -----------------------------------------------------------------------------
 
 /bin/date
 
@@ -15,7 +20,21 @@ set d=`/bin/date +'%Y-%m-%d'`
 
 echo "Today's log names will end in $d.log"
 
-set baseopt="-DOSMOT_CONFIG=."
+echo home=$home
+
+echo arg0=$0
+
+
+if ("$1" == "") then
+else
+    set home=$1
+    echo "Setting home to $home"
+endif
+
+
+#-- OSMOT_CONFIG is the directory where osmot.conf is
+
+set baseopt="-DOSMOT_CONFIG=${home}/arxiv/arxiv"
 
 set lib=$home/arxiv/lib
 
@@ -48,21 +67,23 @@ mv missing.txt missing-${d}.txt
 set opt="-Xmx2048m ${baseopt}"
 echo "Options for ArticleAnalyzer: $opt"
 
-time java $opt $1 $2 $3 edu.rutgers.axs.recommender.ArticleAnalyzer >& allnorms-${d}.log 
+time java $opt  edu.rutgers.axs.recommender.ArticleAnalyzer >& allnorms-${d}.log 
+
+set stoplist=${home}/arxiv/arxiv/WEB-INF/stop200.txt 
 
 #-- Taking care of new articles for Exploration Engine ver. 3
-time java $opt   edu.rutgers.axs.bernoulli.Bernoulli recent >& bernoulli-${d}.log 
+time java $opt -Dstoplist=${stoplist}  edu.rutgers.axs.bernoulli.Bernoulli recent >& bernoulli-${d}.log 
 
 #-- Updating class stats and recommendations for Exploration Engine ver. 4
-time java $opt   edu.rutgers.axs.ee4.Daily update >& ee4-${d}.log 
+time java $opt  -Dstoplist=${stoplist}   edu.rutgers.axs.ee4.Daily update >& ee4-${d}.log 
 
 #-- Updating user profiles and recommendations for the 3PR (PPP) engine
-time java $opt edu.rutgers.axs.recommender.DailyPPP update >& ppp-${d}.log 
+time java $opt  -Dstoplist=${stoplist} edu.rutgers.axs.recommender.DailyPPP update >& ppp-${d}.log 
 
 #-- send emails to subscribers
 time java $opt -Dforce=false -Dsmtp=localhost edu.rutgers.axs.web.EmailSug  >& email-${d}.log 
 
 set  opt="-Xmx1024m ${baseopt} -DexitAfter=22 -DarticlesUpdated=true"
 
-time java $opt $1 $2 $3 edu.rutgers.axs.recommender.TaskMaster >& taskmaster-${d}.log
+time java $opt edu.rutgers.axs.recommender.TaskMaster >& taskmaster-${d}.log
 
