9a278b826b06dcb3ffb6b43a2888ba8d240eac58
lc629 <lc629@86fa5bc5-d9b1-0410-b350-fe26eda8ebdd>
2015-02-20 13:20:12 +0000

    added relevant files for CTPF SBRG method

---
+++ src/edu/rutgers/axs/sb/SBRGLoadCTPFFit.java
@@ -0,0 +1,141 @@
+package edu.rutgers.axs.sb;
+import edu.rutgers.axs.sql.Logging;
+
+import java.io.*;
+import java.net.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+import java.util.zip.GZIPInputStream;
+import javax.persistence.*;
+
+import org.apache.commons.math3.special.Gamma;
+import org.apache.lucene.document.*;
+import org.apache.lucene.index.*;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.ScoreDoc;
+
+
+class SBRGLoadCTPFFit extends Thread {
+
+    String path;
+    CTPFFit ctpffit; 
+
+    SBRGLoadCTPFFit(String _path, CTPFFit _ctpffit)
+    {
+        path = _path; 
+        ctpffit = _ctpffit;
+    }
+
+    public void run()  {
+        loadFit(path);
+        ctpffit.loaded= true; 
+    }
+
+    // load data 
+    private void loadFit(String path) {
+
+        try { 
+           
+            //Logging.info("loading epsilon shape"); 
+            //float [][] epsilon_shape = load(path + "epsilon_shape.tsv.gz"); 
+            //Logging.info("loading epsilon rate"); 
+            //float [][] epsilon_rate = load(path + "epsilon_scale.tsv.gz"); // actually a rate
+
+            Logging.info("Loading fit ");
+            ctpffit.epsilonlog = load(path + "epsilon_log_10K.tsv.gz");
+            ctpffit.thetalog = load(path + "theta_log_10K.tsv.gz");
+            ctpffit.epsilon_plus_theta = load(path + "epsilon_plus_theta_10K.tsv.gz"); 
+
+            // updateExpectationsEpsilonTheta(epsilon_shape, epsilon_rate, epsilonlog);
+
+            // Logging.info("loading theta shape"); 
+            // float [][] theta_shape = load(path + "theta_shape.tsv.gz"); 
+            // Logging.info("loading theta rate"); 
+            // float [][] theta_rate = load(path + "theta_scale.tsv.gz"); // actually a rate
+
+            //theta = new float[theta_shape.length][theta_shape[0].length];
+            // thetalog = new float[theta_shape.length][theta_shape[0].length];
+
+            // updateExpectationsEpsilonTheta(theta_shape, theta_rate, thetalog);
+
+            //epsilon_plus_theta = load(path + "epsilon_plus_theta.tsv.gz"); 
+            // epsilon_plus_theta = new float[thetalog.length][thetalog[0].length]; 
+            // for(int i=0; i<thetalog.length; ++i)
+            //     for(int j=0; j<thetalog[0].length; ++j)
+            //         epsilon_plus_theta[i][j] = epsilon_shape[i][j]/epsilon_rate[i][j] + theta_shape[i][j]/theta_rate[i][j]; 
+
+            // load map 
+            loadMap(path + "items_10K.tsv.gz");
+
+        } catch(Exception ex) { 
+            // TODO: put this back
+	    // error = true;
+	    Logging.info("Exception when loading fit:" + ex.getMessage());
+	    ex.printStackTrace(System.out);
+        }
+    } 
+
+    private void loadMap(String file) throws Exception { 
+        ctpffit.internalID_to_aID = new HashMap<Integer, String>();
+        ctpffit.aID_to_internalID = new HashMap<String, Integer>();
+        GZIPInputStream gzip = new GZIPInputStream(new FileInputStream(file));
+        BufferedReader br = new BufferedReader(new InputStreamReader(gzip));
+
+        String line; 
+        int k=0; 
+        while ((line = br.readLine()) != null) {
+            String[] parts = line.split("\t");
+            ctpffit.internalID_to_aID.put(Integer.parseInt(parts[0]), parts[1]);
+            ctpffit.aID_to_internalID.put(parts[1], Integer.parseInt(parts[0]));
+        }
+        Logging.info("size of internalID_to_aID: " + Integer.toString(ctpffit.internalID_to_aID.size()));
+        Logging.info("size of aID_to_internalID: " + Integer.toString(ctpffit.aID_to_internalID.size()));
+
+        Logging.info("fit loaded"); 
+
+    }
+
+    private float[][] load(String file) throws Exception {
+        float[][] d;
+        
+        Logging.info("Loading " + file); 
+
+        // List<String> lines = Files.readAllLines(Paths.get(file), StandardCharsets.UTF_8);
+        GZIPInputStream gzip = new GZIPInputStream(new FileInputStream(file));
+        BufferedReader br = new BufferedReader(new InputStreamReader(gzip));
+
+        //d = new float[SBRGWorkerCTPF.num_docs][];  // !!! 825708
+        d = new float[SBRGWorkerCTPF.num_docs][SBRGWorkerCTPF.num_components];
+        String line; 
+        int k=0; 
+        while ((line = br.readLine()) != null) {
+            String[] parts = line.split("\t");
+            if ((k+1) % 100000 == 0)  {
+                Logging.info("d: " + Integer.toString(k+1) + " length: " + Integer.toString(d[k].length)); 
+            }
+            int j=0;
+            for(String part: parts) { 
+                if(j>=2)
+                    d[k][j-2] = Float.parseFloat(part);
+                ++j;
+            }
+            ++k;
+
+        }
+        return d;
+    }
+
+    // TODO: Consider doing it offline and then loading epsilon_plus_theta, epsilon_log and theta_log. 
+    //private static void updateExpectationsEpsilonTheta(float[][] a_shape, float[][] a_rate, float[][] a, float[][] alog) {
+    private void updateExpectationsEpsilonTheta(float[][] a_shape, float[][] a_rate, float[][] alog) {
+        for(int i=0; i<alog.length; ++i) { 
+            for(int j=0; j<alog[0].length; ++j) { 
+                //a[i][j] = a_shape[i][j]/a_rate[i][j]; 
+                alog[i][j] = (float)(Gamma.digamma(a_shape[i][j]) - Math.log(a_rate[i][j]));
+            }
+        }
+    }
+
+}
+
---
+++ src/edu/rutgers/axs/sb/SBRGWorkerCTPF.java
@@ -0,0 +1,302 @@
+package edu.rutgers.axs.sb;
+
+import java.io.*;
+import java.lang.Math;
+import java.nio.*;
+import java.util.*;
+import java.text.*;
+import java.net.*;
+import java.util.Random.*;
+import java.util.regex.*;
+import java.util.zip.GZIPInputStream;
+import javax.persistence.*;
+
+import org.apache.lucene.document.*;
+import org.apache.lucene.index.*;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.commons.math3.special.Gamma;
+
+import edu.rutgers.axs.sql.*;
+import edu.rutgers.axs.web.*;
+import edu.rutgers.axs.indexer.*;
+
+class CTPFFit extends Object {
+
+    //public float[][] theta, epsilon; 
+    public float[][] epsilon_plus_theta;
+    public float[][] epsilonlog, thetalog; 
+    //public float[][] epsilon_shape, epsilon_rate;
+    //public float[][] theta_shape, theta_rate; 
+    public HashMap<Integer, String> internalID_to_aID;
+    public HashMap<String, Integer> aID_to_internalID;
+    public boolean loaded;
+} 
+
+/** This is a derivative class of SBRGWorker for the CTPF SB-recommendation method 
+ */
+public class SBRGWorkerCTPF extends  SBRGWorker  {
+
+    static long seed = 3; 
+    //static String path = "/home/lc629/arxiv/fits/nusers120298-ndocs825708-nvocab14000-k250-batch-bin-vb-fa-ldainit-fdp/";
+    static String path = "/home/lc629/arxiv/fits/nusers120298-ndocs825708-nvocab14000-k250-batch-bin-vb-fa-ldainit/";
+    static int num_docs = 10000; // 825708; // 1000; // At the very least this should be read from some config file
+    static int num_components = 250; // same as above
+
+    // The fit itself (not including the user)
+    static CTPFFit ctpffit = new CTPFFit(); 
+
+    // User representation 
+    float[] x, xlog, x_shape, x_rate;
+    float x_shape_prior = (float)0.1; 
+    float x_rate_prior = (float)0.1; 
+    TreeSet<String> viewedArticles;
+    Random rnd; 
+
+    // For convenience 
+    float[] epsilon_plus_theta_sum;
+
+    SBRGWorkerCTPF(SBRGenerator _parent,
+                    int _sbStableOrderMode) {
+        super(SBRGenerator.Method.CTPF, _parent, _sbStableOrderMode);
+
+        if (ctpffit.loaded) { 
+            rnd = new Random(SBRGWorkerCTPF.seed);
+            viewedArticles = new TreeSet<String>(); 
+
+            // Xs
+            x = new float[ctpffit.epsilon_plus_theta[0].length]; 
+            xlog = new float[ctpffit.epsilon_plus_theta[0].length]; 
+            x_shape = new float[ctpffit.epsilon_plus_theta[0].length]; 
+            x_rate = new float[ctpffit.epsilon_plus_theta[0].length]; 
+
+            // 
+            epsilon_plus_theta_sum = new float[ctpffit.epsilon_plus_theta[0].length];
+            for (int j=0; j < epsilon_plus_theta_sum.length; ++j) {
+                epsilon_plus_theta_sum[j] = (float)0.;
+                for (int i=0; i < ctpffit.epsilon_plus_theta.length; ++i) { 
+                    epsilon_plus_theta_sum[j] += ctpffit.epsilon_plus_theta[i][j]; 
+                }
+            }
+
+            initializeX();
+            updateExpectationsX(); 
+        }
+
+        Logging.info("Constructed obj."); 
+    }
+
+    //public static void loadFit();
+    //public static boolean myVar = loadFit();
+
+    public static boolean loadFit() {
+        // load data files 
+        Logging.info("Loading fit"); 
+        //SBRGLoadCTPFFit lf = new SBRGLoadCTPFFit(path, ctpffit);;
+        Logging.info("Created SBRGLoadCTPFFit obj"); 
+        ctpffit.loaded = false; 
+        Thread t = new Thread(new SBRGLoadCTPFFit(path, ctpffit));
+        t.start();
+        Logging.info("Thread loading fit launched."); 
+        return true;
+    }
+
+    private void initializeX() { 
+            for(int i=0; i<x.length; ++i) { 
+                x_shape[i] = x_shape_prior; //  + (float)0.001 * rnd.nextFloat(); // TODO: removed for debugging. May be useful.
+                x_rate[i]  = x_rate_prior  + epsilon_plus_theta_sum[i]; // (float)0.001 * rnd.nextInt(1);
+            }
+            print1DArray(x_shape,"x_shape");
+            print1DArray(x_rate,"x_rate");
+    } 
+
+    private void updateExpectationsX() {
+        for(int i=0; i<x.length; ++i) { 
+            x[i] = x_shape[i]/x_rate[i]; 
+            xlog[i] = (float)(Gamma.digamma(x_shape[i]) - Math.log(x_rate[i]));
+        }
+    }
+
+
+    synchronized void work(EntityManager em, IndexSearcher searcher, int runID, ActionHistory _his)  {
+        Logging.info("Working"); 
+        updateUserProfileWithNewClick(_his);
+        computeCTPFRecList(em, searcher, runID); 
+        super.plid = super.saveAsPresentedList(em).getId();
+    }
+
+    synchronized void updateUserProfileWithNewClick(ActionHistory _his) { 
+
+        int internalID; 
+        float[] x2 = new float[x.length*2]; 
+        Logging.info("Number of articles viewed by user: " + Integer.toString(viewedArticles.size()) ); 
+
+        for(String aid: _his.viewedArticlesActionable) { // process articles in user history 
+            if(!viewedArticles.contains(aid)) {
+                // obtain article internal ID
+                //Logging.info("Looking up article" + aid);
+                if(ctpffit.aID_to_internalID.containsKey(aid)) {
+                    internalID = ctpffit.aID_to_internalID.get(aid);
+                    if (internalID > ctpffit.thetalog.length) {
+                        Logging.info("internalID out of range: " + Integer.toString(internalID)); 
+                        continue; 
+                    }
+                } else { 
+                    Logging.info("Article unknown: " + aid); 
+                    continue;
+                }
+                Logging.info("Viewed article: " + Integer.toString(internalID) + " " + aid); 
+
+                // update X's shape 
+                // need to create an array twice the length of X. 
+                int factor = 1; // !! hack
+                int inference_iterations = 1; 
+                for (int ii=0; ii<inference_iterations; ++ii) { 
+                    if (ii % 1000 == 0) 
+                        Logging.info("Iteration: " + Integer.toString(ii)); 
+                for (int k=0; k < x2.length; ++k) { 
+                    if(k < x.length) { 
+                        //Logging.info("thetalog: " + k + " " + Float.toString(ctpffit.thetalog[internalID][k])); 
+                        x2[k] = xlog[k] + factor*ctpffit.thetalog[internalID][k]; 
+                    } else {
+                        int t = x.length - (x2.length - k);
+                        //Logging.info("epsilonlog: " + t + " " + Float.toString(ctpffit.epsilonlog[internalID][t])); 
+                        x2[k] = xlog[t] + factor*ctpffit.epsilonlog[internalID][t]; 
+                    } 
+                    //Logging.info("x2: " + k + " " + Float.toString(x2[k])); 
+                }
+ 
+                normalize(x2); 
+                updateXshape(x2);
+                if (ii % 1000 == 0) 
+                    print1DArray(x_shape,"x_shape"); 
+                updateExpectationsX();
+                if (ii % 1000 == 0) 
+                    print1DArray(x, "x"); 
+
+                }
+                // no need to update the rate unless epsilon_plus_theta changes
+                // updateXrate(ctpffit.epsilon_plus_theta); 
+                // print1DArray(x_rate,"x_rate"); 
+                viewedArticles.add(aid); 
+            } 
+            // update X's expectation
+            updateExpectationsX();
+            print1DArray(x, "x"); 
+        }
+    } 
+
+    private void updateXshape(float[] x2) {
+        for (int i=0; i<x2.length; ++i) { 
+            x_shape[i % x.length] += x2[i]; 
+        }
+    }
+
+    private float logsum(float[] a) { 
+        float r = (float)0.;
+        for (int i=0; i<a.length; ++i) { 
+            if (a[i] < r)
+              r = r + (float)Math.log(1 + Math.exp(a[i] - r));
+            else
+              r = a[i] + (float)Math.log(1 + Math.exp(r - a[i]));
+        }
+        return r;
+    } 
+
+
+    private void normalize(float[] a) { 
+            float sum = logsum(a); 
+            for(int i=0; i<a.length; ++i) { 
+                a[i] = (float)Math.exp(a[i] - sum);  
+            }
+    }
+
+    /* Double check before using.
+    private void updateXrate(float[] updater) {
+        for(int i=0; i<updater.length; ++i) 
+            x_rate[i] += updater[i];
+    }
+    */
+
+    private void print1DArray(float[] a, String str) { 
+        String xs = "["; 
+        for (int i=0; i<a.length; ++i) {
+            if(i == 163)
+                xs += "****";
+            xs += Float.toString(a[i]);
+            if (i != (a.length-1))
+                xs += ", ";
+        }
+        xs += ']';
+        Logging.info(str + ": " + xs); 
+    }
+
+    /** Generates the list of recommendations based on CTPF */
+    private void computeCTPFRecList(EntityManager em, IndexSearcher searcher, int runID) {
+
+	try {
+
+            Logging.info("Calculating Scores"); 
+            // Do x^T * (epsilon + theta)
+            TreeMap<Float,String> scores = new TreeMap<Float,String>();
+            //String old_value = "";
+            float e; 
+            for (int i=0; i<ctpffit.epsilon_plus_theta.length; ++i) {
+                e = (float)0.;
+                for (int j=0; j<ctpffit.epsilon_plus_theta[0].length; ++j) { 
+                    e += x[j]*(ctpffit.epsilon_plus_theta[i][j]);
+                }
+                //Logging.info("(i,e): (" + Integer.toString(i) + "," + float.toString(e) + ") scores size: " + Integer.toString(scores.size()) + " " + old_value); 
+                scores.put(e, ctpffit.internalID_to_aID.get(i));
+            }
+
+            // Get the results 
+            //NavigableSet sorted_articles = scores.descendingKeySet();
+
+	    int k=0;
+	    //int maxAge = his.viewedArticlesActionable.size();
+
+            Logging.info("Formatting results for SB output"); 
+            //Logging.info("scores size: " + Integer.toString(scores.size()));
+            Vector<ArticleEntry> entries = new Vector<ArticleEntry>();
+            String aid; // article id
+            int topK=20; 
+            String reco_articles = ""; 
+            for(float score: scores.descendingKeySet()) {
+                aid = scores.get(score);
+                if(aid != null) {
+                    reco_articles += scores.get(score) + " (" + Float.toString(score) + ") | ";
+                    ArticleEntry ae = new ArticleEntry(++k, scores.get(score));
+                    ae.setScore(score);
+                    ae.age = k-1; 
+                    entries.add(ae);
+                    ++k;
+                } else { 
+                    Logging.info("article id is null " + score); 
+                }
+                if(k>topK) // TODO: hack-ish, make better
+                    break; 
+	    }
+            Logging.info("adding articles id:" + reco_articles); 
+            if(entries.size() > 0) {
+                Logging.info("Adding entries:" + Integer.toString(entries.size())); 
+                sr = new SearchResults(entries); 
+                super.addArticleDetails(searcher);
+            } else {
+                Logging.info("No new entries to add to the recommender list."); 
+            }
+	    //sr.saveAsPresentedList(em,Action.Source.SB,null,null, null);
+
+	}  catch (Exception ex) {
+	    error = true;
+	    errmsg = ex.getMessage();
+	    Logging.error(""+ex);
+	    System.out.println("Exception for SBRG thread "); //  + super.getId());
+	    ex.printStackTrace(System.out);
+	}
+
+        // apply exclusion list? 
+
+
+    }
+}
